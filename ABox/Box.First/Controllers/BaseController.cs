000dd97bfa6af4c46627f672e4529094d8556e14
杨逢春 <517052727@qq.com>
2021-11-02 23:45:20 +0800

    盒子<开始>

---
+++ ABox/Box.First/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Box.First.Controllers
+{
+    /// <summary>
+    /// 基类控制器
+    /// </summary>
+    [ApiController, Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+
+    } 
+}
---
+++ ABox/Box.First/Controllers/OkController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Box.First.Controllers
+{
+    public class OkController : BaseController
+    {
+        /// <summary>
+        /// 心跳检查方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ContentResult Get()
+        {
+            Console.WriteLine($"心跳检查：", DateTime.Now);
+            //return Ok();
+            return Content("OK");
+        }
+    }
+}
