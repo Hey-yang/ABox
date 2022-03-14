0507f0d182fae8e7ee0672c55070233a08916e00
yang <517052727@qq.com>
2022-03-14 23:55:01 +0800

    新增

---
+++ ABox/Box.Redis/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Box.Redis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpPost, Route("SetValue")]
+        public JsonResult SetValue()
+        {
+            new RedisHelper().SetValue("test", "1234512");
+            return new JsonResult("写入成功test:1234512");
+        }
+
+        [HttpGet, Route("GetValue")]
+        public JsonResult GetValue()
+        {
+            var ss =  new RedisHelper().GetValue("test");
+            return new JsonResult(ss);
+        }
+    }
+}
---
+++ ABox/Box.Redis/RedisHelper.cs
@@ -0,0 +1,46 @@
+﻿using StackExchange.Redis;
+
+namespace Box.Redis
+{
+    public class RedisHelper
+    {
+        private ConnectionMultiplexer redis { get; set; }
+        private IDatabase db { get; set; }
+        public RedisHelper()
+        {
+            redis = ConnectionMultiplexer.Connect("81.68.64.154:8081,password=123456");
+            db = redis.GetDatabase();
+        }
+
+        /// <summary>
+        /// 增加/修改
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool SetValue(string key, string value)
+        {
+            return db.StringSet(key, value);
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetValue(string key)
+        {
+            return db.StringGet(key);
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool DeleteKey(string key)
+        {
+            return db.KeyDelete(key);
+        }
+    }
+}
