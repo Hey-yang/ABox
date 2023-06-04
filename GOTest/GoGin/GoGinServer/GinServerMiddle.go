ec8726799d13d8d961865cdf24fd5eaa52b90635
杨逢春 <517052727@qq.com>
2023-06-04 21:02:21 +0800

    调整

--- GOTest/GoGin/GoGinServer/GinServerMiddle.go
+++ GOTest/GoGin/GoGinServer/GinServerMiddle.go
@@ -2,7 +2,7 @@ package goginserver
 
 import (
 	"fmt"
-	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,19 +10,29 @@ import (
 func GetGinServerSi() *gin.Engine {
 	// 初始化 Gin 框架默认实例，该实例包含了路由、中间件以及配置信息
 	r := gin.Default()
-	// JSON绑定
-	r.POST("loginJSON", func(c *gin.Context) {
-		// new结构体，给结构体分配内存
-		login := new(Login)
-		// 将request的body中的数据，自动按照json格式解析到结构体
-		if err := c.ShouldBindJSON(&login); err != nil {
-			// 返回错误信息
-			// gin.H封装了生成json数据的工具
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		fmt.Println(*login)
-		c.JSON(http.StatusOK, gin.H{"status": "200"})
-	})
+	r.Use(myTime)
+	// {}为了代码规范
+	shoppingGroup := r.Group("/shopping")
+	{
+		shoppingGroup.GET("/index", shopIndexHandler)
+		shoppingGroup.GET("/home", shopHomeHandler)
+	}
 	return r
+}
+
+// 定义中间件
+func myTime(c *gin.Context) {
+	start := time.Now()
+	c.Next()
+	// 统计时间
+	since := time.Since(start)
+	fmt.Println("程序用时：", since)
+}
+
+func shopIndexHandler(c *gin.Context) {
+	time.Sleep(5 * time.Second)
+}
+
+func shopHomeHandler(c *gin.Context) {
+	time.Sleep(3 * time.Second)
 }
